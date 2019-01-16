9081bd0f058ffae7b5ffcb4589d779d5e8ce65f2
AntonioMC95 <antonio.martinez@in2.es>
2019-01-16 09:33:32 +0100

    FIX tabs

---
+++ src/app/modules/tabs/components/form-tab-context/form-tab-context.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Context, ContextRows, TabAutonomia} from '../../resources/tab-class-form';
+
+@Component({
+  selector: 'app-form-tab-context',
+  templateUrl: './form-tab-context.component.html',
+  styleUrls: ['./form-tab-context.component.scss']
+})
+export class FormTabContextComponent implements OnInit {
+
+  @Input() context: ContextRows = new ContextRows();
+  @Output () endForm: EventEmitter<boolean> = new EventEmitter();
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  public emitEnd() {
+    this.endForm.emit();
+  }
+
+}
