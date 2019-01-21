18a4015ea3f8ee46df7748577025f6740d2afccc
AntonioMC95 <antonio.martinez@in2.es>
2019-01-21 10:57:29 +0100

    ADD return page tabs

--- src/app/modules/tabs/components/form-tab/form-tab.component.ts
+++ src/app/modules/tabs/components/form-tab/form-tab.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { EnvironmentMaterial, EnvironmentRelacional, RowsQuest, TabAutonomia} from '../../resources/tab-class-form';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-form-tab',
@@ -8,15 +9,32 @@ import { EnvironmentMaterial, EnvironmentRelacional, RowsQuest, TabAutonomia} fr
 })
 export class FormTabComponent implements OnInit {
   rows: RowsQuest [] = [];
+  closeResult : string;
   @Input() data: TabAutonomia = new TabAutonomia();
   @Input () groupRelacional: EnvironmentRelacional = new EnvironmentRelacional();
   @Input () groupMaterial: EnvironmentMaterial = new EnvironmentMaterial();
   @Output () before: EventEmitter<boolean> = new EventEmitter();
-  constructor() {
+  constructor(private modalService: NgbModal) {
 
   }
 
   ngOnInit() {
+  }
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
   addRow( i: number, ssb: string) {
