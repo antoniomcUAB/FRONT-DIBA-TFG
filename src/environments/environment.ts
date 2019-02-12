edd120df5ef34be68d877646939e84ccee7d3b20
Daniel Meller Soto <daniel.meller@in2.es>
2019-02-12 10:51:20 +0100

    Cambio ruta API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const env = {
   serverURL: 'http://localhost:4200',
   // apiURL   : 'http://10.14.1.165:8080' // LOCAL
-  // apiURL   : 'http://10.14.1.165:7001' // LOCAL
-  apiURL   : 'http://dsdiba-api.demo.in2.es' // DES
+  apiURL   : 'http://10.14.4.52:7001' // LOCAL
+  // apiURL   : 'http://dsdiba-api.demo.in2.es' // DES
 };
 
 export const environment = {
