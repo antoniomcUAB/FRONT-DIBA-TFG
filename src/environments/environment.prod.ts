65e68f293ba3cd82ea5577617b67fe41d1a5054f
Daniel Meller Soto <daniel.meller@in2.es>
2019-02-18 13:08:30 +0100

    CHANGE enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 declare const env: any;
+
 export const environment = {
   production: true,
   serverURL: env.serverURL
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,4 @@
-export const env = {
-  serverURL: 'http://localhost:4200',
-  // apiURL   : 'http://10.14.1.165:8080' // LOCAL
-  // apiURL   : 'http://10.14.4.52:7001' // LOCAL
-  apiURL   : 'http://dsdiba-api.in2.es' // DES
-};
+declare const env: any;
 
 export const environment = {
   production: false,
