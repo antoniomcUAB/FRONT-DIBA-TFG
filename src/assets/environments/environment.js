10f734aaa018db9ec7fd3ea9e791c9b54016d461
antoniomc <antonio.martinez@in2.es>
2019-05-03 13:54:01 +0200

    add localhost 8089

--- src/assets/environments/environment.js
+++ src/assets/environments/environment.js
@@ -1,7 +1,7 @@
 const env = {
   // apiURL   : 'http://10.14.1.165:8080' // LOCAL
   // apiURL   : 'http://10.14.4.52:7001' // LOCAL
-  serverURL: 'http://localhost:4200',
+  serverURL: 'http://localhost:8090',
   apiURL   : 'http://dsdiba.api.demo.in2.es', // DES
   whitelistedDomains: ['dsdiba.api.demo.in2.es'],
   blacklistedRoutes: ['dsdiba.api.demo.in2.es/dsdiba/api/login']/*
