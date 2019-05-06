b4eb9b7c3e14de5d5223b97b4b4d6f5801b635e3
antoniomc <antonio.martinez@in2.es>
2019-05-06 11:51:09 +0200

    add change enviroment

--- src/assets/environments/environment.js
+++ src/assets/environments/environment.js
@@ -2,8 +2,8 @@ const env = {
   // apiURL   : 'http://10.14.1.165:8080' // LOCAL
   // apiURL   : 'http://10.14.4.52:7001' // LOCAL
   serverURL: 'http://localhost:8090',
- // apiURL   : 'http://dsdiba.api.demo.in2.es', // DES
-  apiURL   : 'http://localhost:8090', // DES
+  apiURL   : 'http://dsdiba.api.demo.in2.es', // DES
+  //apiURL   : 'http://localhost:8090', // DES
   whitelistedDomains: ['dsdiba.api.demo.in2.es'],
   blacklistedRoutes: ['dsdiba.api.demo.in2.es/dsdiba/api/login']/*
   apiURL   : 'http://localhost:8081', // DES
