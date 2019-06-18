56e73489a1fa348977c0208d560978b79ed4b9cf
antoniomc <antonio.martinez@in2.es>
2019-06-18 10:08:35 +0200

    CHANGE url environments

--- src/assets/environments/environment.js
+++ src/assets/environments/environment.js
@@ -1,10 +1,10 @@
 const env = {
   // serverURL: 'dsdiba.api.demo.in2.es',  DES
   serverURL: 'http://localhost:4200', //DES
-  apiURL   : 'http://dsdiba.api.demo.in2.es', // DES
+  apiURL   : 'http://su0353.corpo.ad.diba.es', // DES
   // apiURL   : 'http://localhost:8090', // LOCAL
-  whitelistedDomains: ['dsdiba.api.demo.in2.es', 'localhost:8090'],
-  blacklistedRoutes: ['dsdiba.api.demo.in2.es/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']/*
+  whitelistedDomains: ['http://su0353.corpo.ad.diba.es:8030', 'localhost:8090'],
+  blacklistedRoutes: ['http://su0353.corpo.ad.diba.es:8030/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']/*
   apiURL   : 'http://localhost:8081', // DES
   whitelistedDomains: [],
   blacklistedDomains: ['localhost:8081', 'localhost:4200']*/
