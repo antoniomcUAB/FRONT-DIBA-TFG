63003f1247ebbd949ac43e59128834f35f4d1c8d
Luis Rojas <luis.rojas@in2.es>
2019-06-26 13:05:35 +0200

    Actualización diba des cliente - Subimos con la configuración del entorno del cliente.

--- src/assets/environments/environment-des-diba.js
+++ src/assets/environments/environment-des-diba.js
@@ -5,14 +5,8 @@
   blacklistedRoutes: ['dsdiba.api.demo.in2.es/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']
 };*/
 const env = {
-  serverURL: 'http://localhost:4200', //DES
-  apiURL   : 'http://10.14.3.92:8090', // DES
-  whitelistedDomains: ['10.14.3.92:8090', 'localhost:8090'],
-  blacklistedRoutes: ['10.14.3.92:8090/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']
+  serverURL: 'http://su0353.corpo.ad.diba.es:8029',
+  apiURL   : 'http://su0353.corpo.ad.diba.es:8030',
+  whitelistedDomains: ['su0353.corpo.ad.diba.es:8030'],
+  blacklistedRoutes: ['su0353.corpo.ad.diba.es:8030/dsdiba/api/login']
 };
-/*const env = {
-  serverURL: 'http://su0353.corpo.ad.diba.es:8030', //DES
-  apiURL   : 'http://su0353.corpo.ad.diba.es:8030', // DES
-  whitelistedDomains: ['su0353.corpo.ad.diba.es', 'localhost:8090'],
-  blacklistedRoutes: ['su0353.corpo.ad.diba.es:8030/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']
-};*/
