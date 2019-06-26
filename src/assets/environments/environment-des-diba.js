03756b6b0d8bf1bbad3e1f0ea1172d58835f6661
Luis Rojas <luis.rojas@in2.es>
2019-06-26 10:16:59 +0200

    Actualización diba app - Subimos diba con la configuración correcta del entorno de desarrollo.

---
+++ src/assets/environments/environment-des-diba.js
@@ -0,0 +1,18 @@
+/*const env = {
+  serverURL: 'http://localhost:4200', //DES
+  apiURL   : 'http://dsdiba.api.demo.in2.es', // DES
+  whitelistedDomains: ['dsdiba.api.demo.in2.es', 'localhost:8090'],
+  blacklistedRoutes: ['dsdiba.api.demo.in2.es/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']
+};*/
+const env = {
+  serverURL: 'http://localhost:4200', //DES
+  apiURL   : 'http://10.14.3.92:8090', // DES
+  whitelistedDomains: ['10.14.3.92:8090', 'localhost:8090'],
+  blacklistedRoutes: ['10.14.3.92:8090/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']
+};
+/*const env = {
+  serverURL: 'http://su0353.corpo.ad.diba.es:8030', //DES
+  apiURL   : 'http://su0353.corpo.ad.diba.es:8030', // DES
+  whitelistedDomains: ['su0353.corpo.ad.diba.es', 'localhost:8090'],
+  blacklistedRoutes: ['su0353.corpo.ad.diba.es:8030/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']
+};*/
