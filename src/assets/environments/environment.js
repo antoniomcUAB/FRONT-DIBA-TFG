fb2a52260a5903d2bf51aa0638ee353a8dc83413
antoniomc <antonio.martinez@in2.es>
2019-06-28 15:18:32 +0200

    CHANGE environment prod

--- src/assets/environments/environment.js
+++ src/assets/environments/environment.js
@@ -1,9 +1,4 @@
-/*const env = {
-  serverURL: 'http://localhost:4200', //DES
-  apiURL   : 'http://dsdiba.api.demo.in2.es', // DES
-  whitelistedDomains: ['dsdiba.api.demo.in2.es', 'localhost:8090'],
-  blacklistedRoutes: ['dsdiba.api.demo.in2.es/dsdiba/api/login', 'localhost:8090/dsdiba/api/login']
-};*/
+
 const env = {
   serverURL: 'http://su0353.corpo.ad.diba.es:8029',
   apiURL   : 'http://su0353.corpo.ad.diba.es:8030',
